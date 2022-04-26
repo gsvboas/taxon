cea62e7dc3040eaf21473639ad231c6204976a9c
s4mukka <meninosam197@gmail.com>
2022-04-25 22:31:09 -0300

    adiciona artefatos by samuel

--- db/MySQL/artefatos.sql
+++ db/MySQL/artefatos.sql
@@ -1,3 +1,111 @@
+/* Começo Artefatos Samuel */
+CREATE OR REPLACE FUNCTION recupera_corridas_pendentes(cpfNumber text, mes integer, ano integer)
+ 	RETURNS TABLE (
+      id_corrida BIGINT,
+      valor_recebido REAL,
+      valor_total  REAL,
+      data_inicio  TIMESTAMP,
+      data_fim     TIMESTAMP
+ 	)
+  	LANGUAGE plpgsql AS
+$func$
+    BEGIN
+       RETURN QUERY
+       select * from recupera_corridas(cpfNumber, mes, ano) as c where c.data_fim is null; -- Utiliza recupera_corridas feito pelo Áquila
+    END
+$func$;
+
+CREATE OR REPLACE VIEW veiculo_view AS
+    SELECT
+        v.chassi as chassi,
+        v.cor as cor,
+        v.placa as placa,
+        v.ano as ano,
+        v.modelo as modelo,
+        v.marca as marca,
+        v.max_ocupacao as max_ocupacao,
+        g.cep as garagem_cep,
+        g.num as garagem_num,
+        g.num_vaga as garagem_num_vaga,
+        p.id as proprietario_id
+    FROM
+        veiculo v, garagem g, proprietariopossuiveiculo p
+    WHERE v.chassi = g.chassi AND p.chassi = v.chassi;
+ 
+CREATE OR REPLACE FUNCTION recupera_veiculos(propietarioId int)
+	RETURNS TABLE (
+    	chassi VARCHAR(17),
+        cor VARCHAR(255),
+        placa VARCHAR(7),
+        ano INT,
+        modelo VARCHAR(50),
+        marca VARCHAR(50),
+        max_ocupacao INT,
+        garagem_cep CHAR(8),
+        garagem_num INT,
+        garagem_num_vaga INT,
+        proprietario_id INT
+  	)
+	LANGUAGE plpgsql AS
+$func$
+BEGIN
+   RETURN QUERY
+   select * from veiculo_view where veiculo_view.proprietario_id = propietarioId;                  
+END
+$func$;
+    
+CREATE OR REPLACE FUNCTION f_i_veiculo()
+	RETURNS TRIGGER AS
+	$func$
+    BEGIN
+    	INSERT INTO veiculo(chassi, cor, placa, ano, modelo, marca, max_ocupacao)
+        	VALUES(NEW.chassi, NEW.cor, NEW.placa, NEW.ano, NEW.modelo, NEW.marca, NEW.max_ocupacao);
+        INSERT INTO garagem(chassi, cep, num, num_vaga)
+        	VALUES(NEW.chassi, NEW.garagem_cep, NEW.garagem_num, NEW.garagem_num_vaga);
+        INSERT INTO proprietariopossuiveiculo(id, chassi)
+        	VALUES(NEW.proprietario_id, NEW.chassi);
+		RETURN NULL;
+    END
+    $func$ LANGUAGE plpgsql;
+    
+ CREATE OR REPLACE TRIGGER t_i_veiculo INSTEAD OF INSERT ON veiculo_view
+ 	FOR EACH ROW EXECUTE PROCEDURE f_i_veiculo();
+    
+CREATE OR REPLACE FUNCTION f_u_veiculo()
+	RETURNS TRIGGER AS
+	$func$
+    BEGIN
+    	UPDATE veiculo
+        	SET cor = NEW.cor, placa = NEW.placa, ano = NEW.ano, modelo = NEW.modelo,
+           	marca = NEW.marca, max_ocupacao = NEW.max_ocupacao
+		WHERE veiculo.chassi = NEW.chassi;
+        UPDATE garagem
+        	SET cep = NEW.garagem_cep, num = NEW.garagem_num, num_vaga = NEW.garagem_num_vaga
+		WHERE garagem.chassi = NEW.chassi;
+        RETURN NULL;
+    END
+    $func$ LANGUAGE plpgsql;
+    
+ CREATE OR REPLACE TRIGGER t_u_veiculo INSTEAD OF UPDATE ON veiculo_view
+ 	FOR EACH ROW EXECUTE PROCEDURE f_u_veiculo();
+    
+    
+CREATE OR REPLACE FUNCTION f_d_veiculo()
+	RETURNS TRIGGER AS
+	$func$
+    BEGIN
+    	DELETE FROM garagem WHERE garagem.chassi = OLD.chassi;
+        DELETE FROM veiculo WHERE veiculo.chassi = OLD.chassi;
+        DELETE FROM proprietariopossuiveiculo WHERE proprietariopossuiveiculo.chassi = OLD.chassi;
+        RETURN NULL;
+    END
+    $func$ LANGUAGE plpgsql;
+    
+ CREATE OR REPLACE TRIGGER t_d_veiculo INSTEAD OF DELETE ON veiculo_view
+ 	FOR EACH ROW EXECUTE PROCEDURE f_d_veiculo();
+        
+/* Fim Artefatos Samuel */
+
 /* Começo Artefatos Gabriele */
 CREATE OR REPLACE VIEW admin_resumo_de_corridas_view AS
     SELECT
